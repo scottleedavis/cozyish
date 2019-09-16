c00abb326bae70cbaec7f3c114605dba85c0f2c5
Scott Lee Davis <skawtus@gmail.com>
2019-09-15 22:04:39 -0700

    dockerfile

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/scottleedavis/cozyish
+
+go 1.12
+
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/gorilla/sessions v1.2.0
+)
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+//server
+//server page with session id
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+
+func main() {
+
+	store.Options = &sessions.Options{
+		MaxAge:   1,
+		Secure:   false,
+		HttpOnly: false,
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	srv.ListenAndServe()
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	// vars := mux.Vars(r)
+	session, _ := store.Get(r, "cozyish-store")
+	if session.Values["client"] == nil {
+		session.Values["client"] = randomId()
+	}
+	session.Save(r, w)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Why Hello there %s\n", session.Values["client"])
+}
+
+func randomId() string {
+	rand.Seed(time.Now().UnixNano())
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ" +
+		"abcdefghijklmnopqrstuvwxyzåäö" +
+		"0123456789")
+	length := 8
+	var b strings.Builder
+	for i := 0; i < length; i++ {
+		b.WriteRune(chars[rand.Intn(len(chars))])
+	}
+	return b.String()
+}
