66fbd1b57e6e7894e13052d4d77bf86c570e52d2
Scott Lee Davis <skawtus@gmail.com>
2019-10-07 20:48:24 -0700

    crawl pngs

--- crawler/app.py
+++ crawler/app.py
@@ -20,29 +20,23 @@ def index(url):
 	myurl = "http://"+API_URL+"/api/index"
 	req = urllib.request.Request(myurl)
 
+	scheme = 'https://'
 	try:
-		html = urlopen("https://"+url)
-		bs = BeautifulSoup(html, 'html.parser')
-		images = bs.find_all('img', {'src':re.compile('.jpg')})
-		for image in images: 
-			images_output.append(image['src'])
-			req.add_header('Content-Type', 'application/json; charset=utf-8')
-			jsondata = json.dumps({"id": 1, "image": "https://"+url+"/"+image["src"]})
-			jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
-			req.add_header('Content-Length', len(jsondataasbytes))
-			urllib.request.urlopen(req, jsondataasbytes)
+		html = urlopen(scheme+url)
 	except:
-		html = urlopen("http://"+url)
-		bs = BeautifulSoup(html, 'html.parser')
-		images = bs.find_all('img', {'src':re.compile('.jpg')})
-		for image in images: 
-			images_output.append(image['src'])
-			req.add_header('Content-Type', 'application/json; charset=utf-8')
-			jsondata = json.dumps({"id": 1, "image": "http://"+url+"/"+image["src"]})
-			jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
-			req.add_header('Content-Length', len(jsondataasbytes))
-			urllib.request.urlopen(req, jsondataasbytes)
+		scheme = 'http://'
+		html = urlopen(scheme+url)
 
+	bs = BeautifulSoup(html, 'html.parser')
+	images = bs.find_all('img', {'src': lambda s: s.endswith((".jpg", ".jpeg", ".png"))})
+	for image in images: 
+		images_output.append(scheme+url+"/"+image["src"])
+		req.add_header('Content-Type', 'application/json; charset=utf-8')
+		jsondata = json.dumps({"id": 1, "image": scheme+url+"/"+image["src"]})
+		jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
+		req.add_header('Content-Length', len(jsondataasbytes))
+		urllib.request.urlopen(req, jsondataasbytes)
+		
 	return jsonify(images_output)
 
 app.run(host='0.0.0.0', port=4444, debug=True)
